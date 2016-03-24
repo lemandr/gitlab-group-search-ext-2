ea9aaab069fe58272b414cff4f4e7e21744170dd
Lev Matash <lev.matash@gmail.com>
2016-03-25 00:18:57 +0500

    add script to page

--- contentscript.js
+++ contentscript.js
@@ -1,2 +1,14 @@
 var search = document.getElementById("search");
 search.value = "extension";
+var b = document.getElementsByTagName("body")[0];
+var script = document.createElement("script");
+//script.appendChild(document.createTextNode("alert('extention alert')"));
+script.setAttribute("src", "chrome://extentions/cmdojppbplgpfipmndmkapjkicbekndm/ext-script.js");
+
+if (b.appendChild) {
+	//console.log(b.appendChild);
+	b.appendChild(script);	
+}
+else {
+	console.log(b);
+}
